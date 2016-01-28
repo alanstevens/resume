433f18ceeaa4c0055e32c985ba3fac5c59612934
H. Alan Stevens <alanstevens@gmail.com>
2016-01-28 10:55:37 -0500

    split content into partials

--- src/build/templates.js
+++ src/build/templates.js
@@ -3,24 +3,6 @@ var fs = require("fs");
 var _ = require("lodash");
 var Handlebars = require("handlebars");
 
-function registerPartials() {
-  var partialsPath = path.resolve(__dirname, "../partials"),
-    partials = fs.readdirSync(partialsPath);
-
-  partials.forEach(function(partial) {
-    var partialName = partial.substr(2, (partial.lastIndexOf(".") - 2));
-    var partialFilepath = path.resolve(__dirname, "../partials", partial);
-    
-    var partialContent = fs.readFileSync(partialFilepath, "utf-8", function(err) {
-      if (err) {
-        console.log("ERROR registering partial:", partialFilepath, err);
-      }
-
-    });
-    Handlebars.registerPartial(partialName, partialContent);
-  });
-}
-
 function registerHelpers() {
   // #compare - adapted from http://doginthehat.com.au/2012/02/comparison-block-helper-for-handlebars-templates/#comment-44
   Handlebars.registerHelper("compare", function(lval, operator, rval, options) {
@@ -92,5 +74,5 @@ function registerHelpers() {
 
 module.exports.init = function() {
   registerHelpers();
-  registerPartials();
+  // registerPartials();
 };
--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ var path = require("path");
 var fs = require("fs");
 var rimraf = require("rimraf");
 var Handlebars = require("handlebars");
-var Templates = require("./build/templates");
+// var Templates = require("./build/templates");
 
-Templates.init();
+// Templates.init();
+registerPartials();
 
 var templatePath = path.resolve(__dirname, "./index-template.html");
-var fileName = path.resolve(__dirname, "../index.html");
 var templateSource = fs.readFileSync(templatePath, "utf-8");
 var template = Handlebars.compile(templateSource);
 var resumeContent = template(data);
+var fileName = path.resolve(__dirname, "../index.html");
 
 rimraf(fileName,
   function() {
@@ -25,3 +26,21 @@ rimraf(fileName,
     }); // fs.writeFile
   } // anonymous function
 ); //rimraf
+
+function registerPartials() {
+  var partialsPath = path.resolve(__dirname, "./partials"),
+    partials = fs.readdirSync(partialsPath);
+
+  partials.forEach(function(partial) {
+    var partialName = partial.substr(2, (partial.lastIndexOf(".") - 2));
+    var partialFilepath = path.resolve(__dirname, "./partials", partial);
+
+    var partialContent = fs.readFileSync(partialFilepath, "utf-8", function(err) {
+      if (err) {
+        console.log("ERROR registering partial:", partialFilepath, err);
+      }
+
+    });
+    Handlebars.registerPartial(partialName, partialContent);
+  });
+}
